de4a3b4790fd03ac986288c628564103ecca1557
AniketD05 <aniketdhiwar1999@gmail.com>
2024-04-11 22:24:33 +0530

    file update

--- LinkedLists/FindMiddleOfLL.java
+++ LinkedLists/FindMiddleOfLL.java
@@ -26,6 +26,20 @@ public class FindMiddleOfLL {
         head = newNode;
     }
 
+    public void findMiddle() {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        System.out.println("Middle Node  : " + slow.data);
+
+    }
+
     public void display() {
 
         Node n = head;
@@ -41,11 +55,16 @@ public class FindMiddleOfLL {
 
         FindMiddleOfLL ll = new FindMiddleOfLL();
 
-        ll.insert(1);
-        ll.insert(2);
-        ll.insert(3);
-        ll.insert(4);
+        ll.insert(8);
+        ll.insert(7);
+        ll.insert(6);
         ll.insert(5);
+        ll.insert(4);
+        ll.insert(3);
+        ll.insert(2);
+        ll.insert(1);
+       
         ll.display();
+        ll.findMiddle();
     }
 }
