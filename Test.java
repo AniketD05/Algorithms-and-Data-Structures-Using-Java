0296137902495a00eb0ae372e0fc4273ea93f758
AniketD05 <aniketdhiwar1999@gmail.com>
2024-06-15 15:32:53 +0530

    file update

--- Test.java
+++ Test.java
@@ -1,23 +1,39 @@
+
 import java.util.Scanner;
 
 public class Test {
 
-    public static  int factOfNum(int num) {
+    public static int gcd(int num1, int num2) {
 
-        if (num == 0) {
-            return 1;
+        if (num2 == 0) {
+            return num1;
         } else {
-            return factOfNum(num - 1) * num;
+            return gcd(num2, num1 % num2);
         }
+    }
+
+    public static int lcm(int gcd, int num1, int num2) {
+
+        return (num1 * num2) / gcd;
+       
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Number : ");
-        int num = sc.nextInt();
+        System.out.print("Enter First Number : ");
+        int num1 = sc.nextInt();
 
-        System.out.println(factOfNum(num));
+        System.out.print("Enter Second Number : ");
+        int num2 = sc.nextInt();
+
+        int result = gcd(num1, num2);
+
+        int lcm = lcm(result, num1, num2);
+
+        System.out.println(lcm);
+
+        sc.close();
     }
 }
